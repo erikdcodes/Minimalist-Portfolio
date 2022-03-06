cf9a294bbf5e501b063312dd5b8abc92c7e60163
Erik De Jesus <erikdcodes@gmail.com>
2022-03-06 07:11:54 -0800

    init portfolio index and project page template

---
+++ pages/portfolio/[slug].js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+// layout
+// hero image
+// section component
+// project background title
+// project background text
+// static previews
+// links
+// call to action
+
+const Project = () => {
+  return <div>Project</div>;
+};
+
+export default Project;
--- pages/portfolio/index.js
+++ pages/portfolio/index.js
@@ -1,5 +1,5 @@
 const Portfolio = () => {
-  return <div>Portfolio</div>;
+  return <div>Portfolio Index</div>;
 };
 
 export default Portfolio;
