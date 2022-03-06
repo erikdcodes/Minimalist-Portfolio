5479102f2a0f16813a0e8b29ac97dbf2cc942227
Erik De Jesus <erikdcodes@gmail.com>
2022-03-06 10:55:09 -0800

    fake data and project images

--- pages/portfolio/index.js
+++ pages/portfolio/index.js
@@ -1,5 +1,13 @@
-const Portfolio = () => {
+const Portfolio = ({ projects }) => {
   return <div>Portfolio Index</div>;
 };
+
+export async function getStaticProps({}) {
+  const projects = await fetch("");
+
+  return {
+    props: { projects },
+  };
+}
 
 export default Portfolio;
