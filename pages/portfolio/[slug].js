e14f80a115cb56956337c2bd602c4a51ed438b6d
Erik De Jesus <erikdcodes@gmail.com>
2022-03-08 07:59:13 -0800

    add markup

--- pages/portfolio/[slug].js
+++ pages/portfolio/[slug].js
@@ -11,13 +11,38 @@ import getProjects from "../../lib/getProjects";
 // links
 // call to action
 
-const Project = (props) => {
-  // console.log(props);
-  const { project } = props;
-
+const Project = ({ project }) => {
+  console.info(project);
   return (
     <DefaultLayout>
-      <h1>{project.title}</h1>
+      <Wrapper>
+        <div className="image-container">
+          <img src={project.heroImage2} alt={`${project.title} screenshot`} />
+        </div>
+        <section>
+          <section className="description-container">
+            <h1>{project.title}</h1>
+            <p>{project.description}</p>
+            <div className="tags-container">
+              {project.tags.map((tag, i) => {
+                if (i === project.tags.length - 1) {
+                  return <span key={tag + i}>{tag}</span>;
+                } else return <span key={tag + i}>{`${tag} / `}</span>;
+              })}
+            </div>
+          </section>
+          <section className="background-container">
+            <h2>Project Background</h2>
+            <p>{project.background}</p>
+            <div className="previews-container">
+              <h3>Static Previews</h3>
+              {project.images.map((img, i) => (
+                <img key={i + img} src={img} />
+              ))}
+            </div>
+          </section>
+        </section>
+      </Wrapper>
     </DefaultLayout>
   );
 };
